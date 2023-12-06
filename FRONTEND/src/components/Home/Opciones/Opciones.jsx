ad25fb8f2fb2e091ad7e6059df941234a9550b9e
nachof948 <nachof948@gmail.com>
2023-12-06 12:18:51 -0300

    Seccion opciones resposive

--- FRONTEND/src/components/Home/Opciones/Opciones.jsx
+++ FRONTEND/src/components/Home/Opciones/Opciones.jsx
@@ -2,6 +2,7 @@ import React,{useState, useEffect} from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { mirarProducto } from '../../../Funciones/mirarProducto';
+import { agregarAlCarrito } from '../../../Funciones/agregarProducto';
 import './Hojas de estilo/Opciones.css'
 
 const Opciones = () => {
@@ -16,18 +17,6 @@ const Opciones = () => {
     
   },[])  
   
-  const agregarAlCarrito = (productoId) => {
-    // Realizar una solicitud al servidor para agregar el producto al carrito por su ID
-    axios.post('/compras/agregar', { productoId })
-      .then((response) => {
-        navegar('/compras')
-        console.log('Producto agregado al carrito', response.data.carrito);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
 
   return(
     <section id='opciones' className='seccion-opciones' >
@@ -35,7 +24,7 @@ const Opciones = () => {
         {opciones.slice(0, 4).map(opcion =>{
           const {nombre, descripcion, precio,imgUrl, _id} = opcion
           return(
-            <div className='tarjeta tarjeta-producto'key={_id}>
+            <div key={_id}>
             <div className='tarjeta tarjeta-producto' onClick={()=>{mirarProducto(_id, navegar)}} > 
               <img src={imgUrl} alt={nombre} loading="lazy" />
               <div className="tarjeta-textos">

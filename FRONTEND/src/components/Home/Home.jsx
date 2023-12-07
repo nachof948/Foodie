b6a85f94adba3690996b4143e6c4175b41f35051
nachof948 <nachof948@gmail.com>
2023-12-07 09:36:47 -0300

    Spinners en el home

--- FRONTEND/src/components/Home/Home.jsx
+++ FRONTEND/src/components/Home/Home.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Elegirnos, Footer, Header, Hero, MenuEspecial, MenuHabitual, Opciones, SobreNosotros } from '../../indice';
+import './Hero/Hojas de estilo/Hero.css'
+const Home = ({ userGoogle }) => {
+  const [loading, setLoading] = useState(true);
 
-const Home = ({userGoogle}) => {
-  return(
+  useEffect(() => {
+    // Simulando una carga de datos con un temporizador
+    const timer = setTimeout(() => {
+      setLoading(false); // Una vez que se simula la carga, establecer loading en falso
+    }, 2000); // Tiempo de carga simulado de 2 segundos
+
+    return () => clearTimeout(timer); // Limpiar el temporizador si el componente se desmonta antes de que termine la carga
+  }, []);
+
+  return (
     <>
-      <Header userGoogle={userGoogle}/>
-      <Hero />
-      <Opciones />
-      <Elegirnos />
-      <MenuEspecial />
-      <MenuHabitual />
-      <SobreNosotros />
-      <Footer />
+      {loading ? (
+        <div className='contenedor-spinner-home'>
+            <h1>FOODIED</h1>
+            <div className="spinner"></div>
+        </div>
+        
+      ) : (
+        <>
+          <Header userGoogle={userGoogle} />
+          <Hero />
+          <Opciones />
+          <Elegirnos />
+          <MenuEspecial />
+          <MenuHabitual />
+          <SobreNosotros />
+          <Footer />
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export { Home }
+export { Home };

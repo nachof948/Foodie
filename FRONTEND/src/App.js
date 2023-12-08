8b61462c944a1b5fa71bef84b1382831a8773a36
nachof948 <nachof948@gmail.com>
2023-12-08 12:52:26 -0300

    Pruebas

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -2,14 +2,29 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { Route, Routes} from 'react-router-dom'
 import { TodasLasComidas, Carnes, Ensaladas, Sushi, Pastas, Pizzas, Sopas, Dulces, Veganos, Hamburguesas, Home, Producto, Registrarme, Carrito, CompraRealizada} from './indice';
-import axios from 'axios';
 function App() {
   const [userGoogle, setUserGoogle] = useState(null)
   
   useEffect(() => {
-    const obtenerUsuario = async () => {
-      try {
-        const response = await axios.get('https://restaurante-foodied.onrender.com/auth/exito');
+    const obtenerUsuario = () => {
+      
+        fetch('https://restaurante-foodied.onrender.com/auth/exito',{
+          method: "GET",
+          credentials:"include",
+          headers:{
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Credentials": true
+          },
+          }).then((response) =>{
+            if (response.status === 200) return response.json()
+            throw new Error("La autentificacion fallo")
+          }).then(resObject =>{
+            setUserGoogle(resObject.user)
+          }).catch(err =>{
+            console.log(err)
+          })
+        /* const response = await axios.get('https://restaurante-foodied.onrender.com/auth/exito');
         
         if (response.status === 200) {
           if (response.data && response.data.user) {
@@ -19,10 +34,8 @@ function App() {
           }
         } else {
           throw new Error('Error en la autentificación');
-        }
-      } catch (err) {
-        console.log(err);
-      }
+        } */
+
     };
   
     obtenerUsuario();

5b7b3f416076e5d071e88986b57598324b48aa4d
nachof948 <nachof948@gmail.com>
2023-12-08 08:24:59 -0300

    Cambios

--- BACKEND/config/config.js
+++ BACKEND/config/config.js
@@ -7,7 +7,7 @@ passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
     callbackURL:"https://restaurante-foodied.onrender.com/auth/google/callback",
-    },
+    } ,
     async (accessToken, refreshToken, profile, done) =>{
         try{
             const usuarioRegistrado = await ClienteGoogle.findOne({googleId: profile.id})

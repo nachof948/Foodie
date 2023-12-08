b1c2f9c6ad710bf163adf82f5e06f93234f701ad
nachof948 <nachof948@gmail.com>
2023-12-07 22:37:55 -0300

    Cambios URL

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -17,7 +17,7 @@ const GooglePassport = require('./config/config')
 
 app.use(
     cors({
-    origin: ['http://localhost:3000'],
+    origin: "https://restaurant-foodied.onrender.com",
     methods: 'GET, POST, PUT, DELETE',
     credentials: true
     })

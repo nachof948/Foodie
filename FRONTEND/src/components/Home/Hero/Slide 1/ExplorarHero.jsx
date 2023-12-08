b7f7cd7900fc501c0918bbc5734b355bcbf3d5e4
nachof948 <nachof948@gmail.com>
2023-12-07 22:56:13 -0300

    Cambios

--- FRONTEND/src/components/Home/Hero/Slide 1/ExplorarHero.jsx
+++ FRONTEND/src/components/Home/Hero/Slide 1/ExplorarHero.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const ExplorarHero = () => {
   return(
     <>
-      <Link to={"/comidas/all"} className='btn-hero-der'>Explorar Mas <span><i className="bi bi-arrow-right-circle"></i></span></Link>
+      <Link to={"https://restaurant-foodied.onrender.com/comidas/all"} className='btn-hero-der'>Explorar Mas <span><i className="bi bi-arrow-right-circle"></i></span></Link>
     </>
   )
 }

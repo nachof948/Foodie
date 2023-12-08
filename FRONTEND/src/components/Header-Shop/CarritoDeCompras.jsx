81952ed4ae57a2ba6faeae2612d2af3fb21b1250
nachof948 <nachof948@gmail.com>
2023-12-07 23:16:43 -0300

    Cambios

--- FRONTEND/src/components/Header-Shop/CarritoDeCompras.jsx
+++ FRONTEND/src/components/Header-Shop/CarritoDeCompras.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const CarritoDeCompras = () => {
   return(
     <div>
-      <Link to={"https://restaurante-foodied.onrender.com/compras"}><i class="bi bi-cart"></i></Link>
+      <Link to={"https://restaurant-foodied.onrender.com/compras"}><i class="bi bi-cart"></i></Link>
     </div>
   )
 }

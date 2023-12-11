0c5ba1690f00c6e272ff9f2fe2b29e812b1621e6
nachof948 <nachof948@gmail.com>
2023-12-11 12:44:25 -0300

    pruebas

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -15,7 +15,7 @@ function App() {
         console.log('El usuario es:', response.data)
         if (response.status === 200) {
           if (response.data) { 
-            setUserGoogle(response.data.user); // Usuario autenticado
+            setUserGoogle(response.data); // Usuario autenticado
           } else {
             setUserGoogle(null); // Usuario no autenticado
           }

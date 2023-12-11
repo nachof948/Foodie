802ac3465bf0482baf5b91b6c9d2cc8f7e71cc78
nachof948 <nachof948@gmail.com>
2023-12-11 18:32:01 -0300

    Cambios

--- FRONTEND/src/components/Sign up - Sign in/Google.jsx
+++ FRONTEND/src/components/Sign up - Sign in/Google.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Google = () => {
   
   const googleAuth = ()=>{
-    window.open("https://restaurante-foodied.onrender.com/auth/google","_self")
+    window.open("/auth/google","_self")
   }
 
   return(

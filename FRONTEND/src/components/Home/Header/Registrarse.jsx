df6efb45e56d4fc7c7b54abd798fac5626f66a3a
nachof948 <nachof948@gmail.com>
2023-12-13 08:59:42 -0300

    cambios

--- FRONTEND/src/components/Home/Header/Registrarse.jsx
+++ FRONTEND/src/components/Home/Header/Registrarse.jsx
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-
+import { Link } from 'react-router-dom'
 const Registrarse = ({ userGoogle }) => {
   const [logout, setLogout] = useState(false)
   
@@ -15,7 +15,7 @@ const Registrarse = ({ userGoogle }) => {
     <>
       {!userGoogle && (
         <div className='registrarse'>
-          <a href={"https://restaurant-foodied.onrender.com/auth/registrarse"}>Registrarse</a>
+          <Link to={"https://restaurant-foodied.onrender.com/auth/registrarse"}>Registrarse</Link>
         </div>
       )}
       {userGoogle && (
